a554b5e0eb4c073c8c8b5444b293770c19f78f14
Judy <gaga.lee.tpi@cdfhgroup.onmicrosoft.com>
2022-12-02 19:17:25 -0800

    update api_path address

--- config/API_PATH.js
+++ config/API_PATH.js
@@ -1,7 +1,11 @@
 module.exports = {
   TEST: '/test',
   RATE: '/rate',
+<<<<<<< HEAD
   RATE_UPDATE: '/rate/update',
   GET_RATE_BY_USER: '/rate/get',
   ORDER: '/order',
+=======
+  ADDRESS: '/address',
+>>>>>>> 2794a07 (update api_path address)
 };
