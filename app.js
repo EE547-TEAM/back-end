9a6f0f17a14b9b1fb9caaf6c0feef76c57f1fc7e
ChenhuaFan <chenhuaf@usc.edu>
2022-12-02 18:27:45 -0800

    update express.urlencoded to ture

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ async function startApp() {
 
   app.use(logger('dev'));
   app.use(express.json());
-  app.use(express.urlencoded({ extended: false }));
+  app.use(express.urlencoded({ extended: true }));
   app.use(cookieParser());
   app.use(express.static(path.join(__dirname, 'public')));
 
