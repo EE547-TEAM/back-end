3e7e5d8bef40e785a864e220b9300d4effcc8389
Judy <gaga.lee.tpi@cdfhgroup.onmicrosoft.com>
2022-12-02 22:49:51 -0800

    address undefined filter

--- routes/api/v1/handler/address.js
+++ routes/api/v1/handler/address.js
@@ -9,6 +9,7 @@
  * visit https://expressjs.com/en/starter/basic-routing.html for details.
  */
 const express = require('express');
+const { isObjectIdOrHexString } = require('mongoose');
 const { ADDRESS } = require('../../../../config/API_PATH');
 const { addressCreate, getAddressbyId } = require('../../../../libs/apis/graphql');
 
@@ -18,17 +19,21 @@ router.post(ADDRESS, (req, res) => {
   const {
     content, userId, def,
   } = req.body;
-  if (content !== '' || userId !== '') {
-    addressCreate({
-      content, userId, def,
-    });
+  if (content !== '' || content === undefined || userId !== '' || !isObjectIdOrHexString(userId) || userId === undefined || def === undefined) {
+    res.send(400);
   }
+  addressCreate({
+    content, userId, def,
+  });
   res.send(200);
 });
 router.get(ADDRESS, (req, res) => {
   const {
     userId,
   } = req.body;
+  if (userId !== '' || !isObjectIdOrHexString(userId) || userId === undefined) {
+    res.send(400);
+  }
   getAddressbyId({
     userId,
   });
