aa9b583302ae0e1edaf8f9c55acec84222bcc1bb
Judy <gaga.lee.tpi@cdfhgroup.onmicrosoft.com>
2022-12-11 16:22:17 -0800

    add chat

--- libs/mongo/chat.js
+++ libs/mongo/chat.js
@@ -5,18 +5,34 @@
  * https://mongoosejs.com/docs/models.html
  */
 // eslint-disable-next-line no-unused-vars
-const { Document } = require('mongoose');
+const { model, Document, Schema } = require('mongoose');
 const { Chat } = require('./model');
+
+async function chatCreate({ participantId }) {
+  const chat = new Chat({
+    participant: participantId,
+  });
+  const savedDoc = chat.save();
+  return savedDoc;
+}
 
 /**
  *
  * @param {*} param0
  * @returns
  */
-async function findProductbyChat({ email }) {
+async function getChatsbyUserId({ userId }) {
+  const chats = await Chat.where('participant').elemMatch({ $eq: userId }).exec();
+  console.log(chats);
+  return (chats);
+}
+
+async function GetChatbyUser({ email }) {
   return Chat.find({ email });
 }
 
 module.exports = {
-  findProductbyChat,
+  chatCreate,
+  getChatsbyUserId,
+  GetChatbyUser,
 };
